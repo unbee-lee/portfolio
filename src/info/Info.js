1603aa3fb66840262843dc401ea0b3424d266f5e
unbee-lee <246922941+unbee-lee@users.noreply.github.com>
2026-03-16 15:56:51 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/info/Info.js
@@ -0,0 +1,125 @@
+import self from "../img/self.png"
+import mock1 from "../img/mock1.png"
+import mock2 from "../img/mock2.png"
+import mock3 from "../img/mock3.png"
+import mock4 from "../img/mock4.png"
+import mock5 from "../img/mock5.png"
+
+/* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
+how to make this template work best for you! */
+
+export let colors = ["rgba(255, 217, 0, 1)", "rgba(104, 240, 255, 1)"];
+/*
+I highly recommend using a gradient generator like https://gradientgenerator.paytonpierce.dev/ to generate a pair of colors that you like.
+These colors will be used to style your name on the homepage, the background of your picture, and some other accents throughout
+the site.
+ */
+
+/* 
+This variable will change the layout of the website from multipage to single, scrollable page
+*/
+export let singlePage = false;
+
+/*
+So let's get started! Some of the info below is pretty self-explanatory, like 'firstName' and 'bio'. I'll try to explain anything
+that might not be obvious right off the bat :) I recommend looking at the template example live using "npm start" to get an idea
+of what each of the values mean.
+ */
+
+export const info = {
+    firstName: "Eunbee",
+    lastName: "Lee",
+    initials: "el", // the example uses first and last, but feel free to use three or more if you like.
+    position: "a Full Stack Developer",
+    selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
+    gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
+    baseColor: colors[0],
+    miniBio: [ // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
+        {
+            emoji: '☕',
+            text: 'fueled by iced latte'
+        },
+        {
+            emoji: '🌎',
+            text: 'based in Perth'
+        },
+        {
+            emoji: "💼",
+            text: "Master of Computing student at Curtin Uni"
+        },
+        {
+            emoji: "📧",
+            text: "eblee.dev@gmail.com"
+        }
+    ],
+    socials: [
+        {
+            link: "https://github.com/unbee-lee",
+            icon: "fa fa-github",
+            label: 'github'
+        },
+        {
+            link: "https://www.linkedin.com/in/eunbee-lee-905238220/",
+            icon: "fa fa-linkedin",
+            label: 'linkedin'
+        }
+// Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
+// Just change the links so that they lead to your social profiles.
+
+    ],
+    bio: "Hello! I'm Eunbee. I'm a Master of Computing student at Curtin Uni. I'm from Korea and enjoy iced latte even in winter, and believe developers never die — they just refactor. Maybe you should hire me!",
+    skills:
+        {
+            proficientWith: ['python', 'sql', 'git', 'github', 'html5', 'css3'],
+            exposedTo: ['nodejs', 'java', 'javascript', 'Django', 'Springboot', 'Pandas', 'Scikit-learn', 'Jupyter notebook']
+        }
+    ,
+    hobbies: [
+        {
+            label: 'reading',
+            emoji: '📖'
+        },
+        {
+            label: 'webtoon',
+            emoji: '📱'
+        },
+        {
+            label: 'football',
+            emoji: '⚽️'
+        }
+// Same as above, change the emojis to match / relate to your hobbies or interests.
+// You can also remove the emojis if you'd like, I just think they look cute :P
+    ],
+    portfolio: [ // This is where your portfolio projects will be detailed
+        {
+            title: "Theme park simulation",
+            live: "https://github.com/unbee-lee/Themepark-Simulation", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
+            source: "https://github.com/unbee-lee/Themepark-Simulation", // this should be a link to the **repository** of the project, where the code is hosted.
+            image: mock1
+        },
+        {
+            title: "Coming soon",
+            live: "https://github.com/unbee-lee/Building-a-classification-model",
+            source: "https://github.com/unbee-lee/Building-a-classification-model",
+            image: mock2
+        }
+        // {
+        //     title: "Project 3",
+        //     live: "https://paytonpierce.dev",
+        //     source: "https://github.com/paytonjewell",
+        //     image: mock3
+        // },
+        // {
+        //     title: "Project 4",
+        //     live: "https://paytonpierce.dev",
+        //     source: "https://github.com/paytonjewell",
+        //     image: mock4
+        // },
+        // {
+        //     title: "Project 5",
+        //     live: "https://paytonpierce.dev",
+        //     source: "https://github.com/paytonjewell",
+        //     image: mock5
+        // }
+    ]
+}
